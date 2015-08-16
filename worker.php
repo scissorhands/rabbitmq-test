ed1d9eaef2de76847d1a5f0a338a4c374269f2e1
Edward Scissorhands <edward_beats@hotmail.com>
2015-08-16 16:11:27 -0500

    persistency set to true

--- worker.php
+++ worker.php
@@ -14,7 +14,6 @@ $callback = function($msg){
   $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
 };
 
-$channel->basic_qos(null, 1, null);
 $channel->basic_consume('task_queue', '', false, false, false, false, $callback);
 
 while(count($channel->callbacks)) {
