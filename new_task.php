635011ac64c804fd02199f63a283d46f8564ca95
Edward Scissorhands <edward_beats@hotmail.com>
2015-08-16 20:58:52 -0500

    why isnt this the default!?

--- new_task.php
+++ new_task.php
@@ -6,7 +6,7 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('task_queue', false, false, false, false);
+$channel->queue_declare('task_queue_two', false, true, false, false);
 
 $data = implode(' ', array_slice($argv, 1));
 if(empty($data)) $data = "Hello World!";
@@ -14,7 +14,7 @@ $msg = new AMQPMessage($data,
 	array('delivery_mode' => 2) # make message persistent
 );
 
-$channel->basic_publish($msg, '', 'task_queue');
+$channel->basic_publish($msg, '', 'task_queue_two');
 
 echo " [x] Sent ", $data, "\n";
 
--- worker.php
+++ worker.php
@@ -4,7 +4,7 @@ use PhpAmqpLib\Connection\AMQPConnection;
 $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('task_queue_two', false, true, false, false);
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
 $callback = function($msg){
@@ -14,7 +14,8 @@ $callback = function($msg){
   $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
 };
 
-$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+$channel->basic_qos(null, 1, null);
+$channel->basic_consume('task_queue_two', '', false, false, false, false, $callback);
 
 while(count($channel->callbacks)) {
     $channel->wait();
